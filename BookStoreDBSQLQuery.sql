742473a6b27ec1b4a63117d3410734d6e95d5c41
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-05 11:59:25 +0530

    [Saddam]Add: Creating a stored procedure for inserting data into signUp table

--- BookStoreDBSQLQuery.sql
+++ BookStoreDBSQLQuery.sql
@@ -4,7 +4,7 @@ Use BookStoreDB
 --Creating Table of User Details for User SignUp
 --===================================================================================
 Create Table User_SignUp_Table(
-UserId int IDENTITY(1,1) Primary Key,
+UserId int IDENTITY(1,1) Primary Key NOT NULL,
 FullName varchar(20) NOT NULL,
 EmailId varchar(50) NOT NULL,
 Password varchar(20) NOT NULL,
