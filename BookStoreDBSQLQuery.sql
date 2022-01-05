1fc05c6139ec5067a044fc04c4f131cd65dc1489
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-05 11:54:03 +0530

    [Saddam]Add: Creating Table of User Details for User SignUp

--- BookStoreDBSQLQuery.sql
+++ BookStoreDBSQLQuery.sql
@@ -1,2 +1,12 @@
 Create DataBase BookStoreDB
 Use BookStoreDB
+--===================================================================================
+--Creating Table of User Details for User SignUp
+--===================================================================================
+Create Table User_SignUp_Table(
+UserId int IDENTITY(1,1) Primary Key,
+FullName varchar(20) NOT NULL,
+EmailId varchar(50) NOT NULL,
+Password varchar(20) NOT NULL,
+MobileNum bigint NOT NULL
+)
