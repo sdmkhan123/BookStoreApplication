5a56c321fec230db0d92649ee1e0eeaec513cd54
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-05 11:50:05 +0530

    [Saddam]Add: Crates signUp model of user

--- BookStoreModels/SignUpModel.cs
+++ BookStoreModels/SignUpModel.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookStoreModels
 {
-    class SignUpModel
+    public class SignUpModel
     {
+        [Key]
+        public int UserID { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public long MobileNum { get; set; }
     }
 }
