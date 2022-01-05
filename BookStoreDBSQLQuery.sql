189d52efa6f7e66d4199fa02b1dd0021dc314dd0
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-05 11:56:50 +0530

    [Saddam]Add: Creating a stored procedure for inserting data into signUp table

--- BookStoreDBSQLQuery.sql
+++ BookStoreDBSQLQuery.sql
@@ -10,3 +10,18 @@ EmailId varchar(50) NOT NULL,
 Password varchar(20) NOT NULL,
 MobileNum bigint NOT NULL
 )
+--===================================================================================
+-- Creating a stored procedure for SignUp the users / Inserting data into Table
+--===================================================================================
+Create procedure SignUpUsers
+(
+@FullName VARCHAR(20),
+@EmailId VARCHAR(50),
+@Password VARCHAR(20),
+@MobileNum bigint
+)   
+as
+Begin
+Insert into User_SignUp_Table (FullName,EmailId,Password, MobileNum)
+Values (@FullName,@EmailId,@Password, @MobileNum)
+End
