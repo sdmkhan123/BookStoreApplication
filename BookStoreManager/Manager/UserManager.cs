8592a6ffa2ea9d24edd39646d1c3399249f1d53f
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-06 07:42:02 +0530

    [Saddam]Add: Writes logic of User signUp API

--- BookStoreManager/Manager/UserManager.cs
+++ BookStoreManager/Manager/UserManager.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using BookStoreManager.Interface;
+using BookStoreModels;
+using BookStoreRepository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BookStoreManager.Manager
 {
-    class UserManager
+    public class UserManager : IUserManager
     {
+        private readonly IUserRepository repository;
+        public UserManager(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+        public string UserSignUp(SignUpModel signUpModel)
+        {
+            try
+            {
+                return this.repository.UserSignUp(signUpModel);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
