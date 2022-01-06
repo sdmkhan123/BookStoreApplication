e92922a609cee73fd47e46b3f9ea81c1bae0de47
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-06 14:11:59 +0530

    [Saddam]Refactor: Removing unnecessary nameSpaces

--- BookStoreApp/Startup.cs
+++ BookStoreApp/Startup.cs
@@ -4,15 +4,10 @@ using BookStoreRepository.Interface;
 using BookStoreRepository.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookStoreApp
 {
