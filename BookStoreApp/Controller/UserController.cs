098d58ae4e99878571f197253929fcea53c5bb0b
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-06 07:44:35 +0530

    [Saddam]Add: Created a User signUp API

--- BookStoreApp/Controller/UserController.cs
+++ BookStoreApp/Controller/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStoreManager.Interface;
+using BookStoreModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,32 @@ namespace BookStoreApp.Controller
 {
     public class UserController : ControllerBase
     {
+        private readonly IUserManager manager;
+
+        public UserController(IUserManager manager)
+        {
+            this.manager = manager;
+        }
+        [HttpPost]
+        [Route("api/signup")]
+        public IActionResult Register([FromBody] SignUpModel signUpModel)
+        {
+            try
+            {
+                string result = this.manager.UserSignUp(signUpModel);
+                if (result.Equals("Registration is successful"))
+                {
+                    return this.Ok(new ResponseModel<string>() { Status = true, Message = result });
+                }
+                else
+                {
+                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = result });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
+            }
+        }
     }
 }
