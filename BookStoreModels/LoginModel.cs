cfadde957cd6ea59bca51acf41d48708451c06e1
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-07 00:15:56 +0530

    [Saddam]Add: Creates Login Model class

---
+++ BookStoreModels/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookStoreModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string EmailId { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
