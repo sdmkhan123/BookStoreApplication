2d8071e329c75e4d49269732b5ddb5119a38d327
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-08 02:25:27 +0530

    [Saddam]Add: Creates a Reset Password Model

---
+++ BookStoreModels/ResetPasswordModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreModels
+{
+    public class ResetPasswordModel
+    {
+        [Required]
+        public string EmailId { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
