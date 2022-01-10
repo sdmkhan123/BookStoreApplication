5555342e06ecf2415a2a6e0fb35d132c22af42ff
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-11 01:25:39 +0530

    [Saddam]Add: Implemented ForgotPassword API and refactor the code

--- BookStoreApp/Controller/UserController.cs
+++ BookStoreApp/Controller/UserController.cs
@@ -14,6 +14,7 @@ namespace BookStoreApp.Controller
         {
             this.manager = manager;
         }
+
         [HttpPost]
         [Route("api/signup")]
         public async Task<IActionResult> UserSignUp([FromBody] SignUpModel signUpModel)
@@ -23,18 +24,19 @@ namespace BookStoreApp.Controller
                 int result = await this.manager.UserSignUp(signUpModel);
                 if (result !=0)
                 {
-                    return this.Ok(new ResponseModel<string>() { Status = true, Message = "Registration is successful" });
+                    return this.Ok(new { Status = true, Message = "Registration is successful", data = result });
                 }
                 else
                 {
-                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = "Registration is Unsuccessful" });
+                    return this.BadRequest(new { Status = false, Message = "Registration is Unsuccessful", data = result });
                 }
             }
             catch (Exception ex)
             {
-                return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
+                return this.NotFound(new { Status = false, Message = ex.Message });
             }
         }
+
         [HttpPost]
         [Route("api/login")]
         public async Task<IActionResult> Login(LoginModel loginModel)
@@ -50,7 +52,7 @@ namespace BookStoreApp.Controller
                 {
                     return this.BadRequest(new ResponseModel<string>() { Status = false, Message = "Password is incorrect and Login is Unsuccessful" });
                 }
-                else 
+                else
                 {
                     return this.BadRequest(new ResponseModel<string>() { Status = false, Message = "Email is incorrect and Login is Unsuccessful" });
                 }
@@ -60,6 +62,7 @@ namespace BookStoreApp.Controller
                 return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
             }
         }
+
         [HttpPut]
         [Route("api/resetpassword")]
         public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordModel resetPasswordModel)
@@ -69,16 +72,38 @@ namespace BookStoreApp.Controller
                 int result = await this.manager.ResetPassword(resetPasswordModel);
                 if (result!=0)
                 {
-                    return this.Ok(new ResponseModel<string>() { Status = true, Message = "Password reset is successful" });
+                    return this.Ok(new { Status = true, Message = "Password reset is successful" });
                 }
                 else
                 {
-                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = "The Email Id does not exist. Password reset is unsuccessful" });
+                    return this.BadRequest(new { Status = false, Message = "The Email Id does not exist. Password reset is unsuccessful" });
                 }
             }
             catch (Exception ex)
             {
-                return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
+                return this.NotFound(new { Status = false, Message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("api/forgotpassword")]
+        public IActionResult ForgotPassword(string Email)
+        {
+            try
+            {
+                int result = this.manager.ForgotPassword(Email);
+                if (result == 1)
+                {
+                    return this.Ok(new { Status = true, Message = "Email is sent successfully", data = result });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = "Email Id does not exist", data = result });
+                    }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = false, Message = ex.Message });
             }
         }
     }
