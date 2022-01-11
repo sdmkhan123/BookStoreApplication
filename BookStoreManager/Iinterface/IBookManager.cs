acb566c3621ef7ed9a5b5010b1dbecf5ba8b949b
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-11 08:02:33 +0530

    [Saddam]Add: Creates IBookManager BookManager classs and Implement Book add API

---
+++ BookStoreManager/Iinterface/IBookManager.cs
@@ -0,0 +1,9 @@
+﻿using BookStoreModels;
+
+namespace BookStoreManager.Interface
+{
+    public interface IBookManager
+    {
+        int AddBook(BookModel bookModel);
+    }
+}
---
+++ BookStoreManager/Manager/BookManager.cs
@@ -0,0 +1,29 @@
+﻿using BookStoreManager.Interface;
+using BookStoreModels;
+using BookStoreRepository.Interface;
+using System;
+
+namespace BookStoreManager.Manager
+{
+    public class BookManager : IBookManager
+    {
+
+        private readonly IBookRepository bookRepository;
+
+        public BookManager(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+        public int AddBook(BookModel bookModel)
+        {
+            try
+            {
+                return this.bookRepository.AddBook(bookModel);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
