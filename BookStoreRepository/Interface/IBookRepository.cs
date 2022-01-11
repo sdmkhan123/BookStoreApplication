44158bc9636183d7e63d82ab1c8be639e1c1a59c
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-11 08:00:58 +0530

    [Saddam]Add: Creates BookStoreRepository IBookRepository and Implement Book add API

---
+++ BookStoreRepository/Interface/IBookRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStoreModels;
+
+namespace BookStoreRepository.Interface
+{
+    public interface IBookRepository
+    {
+        int AddBook(BookModel bookModel);
+    }
+}
---
+++ BookStoreRepository/Repository/BookRepository.cs
@@ -0,0 +1,53 @@
+﻿using BookStoreModels;
+using BookStoreRepository.Interface;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+
+namespace BookStoreRepository.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        public IConfiguration Configuration { get; }
+        public BookRepository(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        SqlConnection sqlConnection;
+
+        public int AddBook(BookModel bookModel)
+        {
+            sqlConnection = new SqlConnection(this.Configuration.GetConnectionString("BookStoreDB"));
+            try
+            {
+                using (sqlConnection)
+                {
+                    string storeprocedure = "Sp_AddBooks";
+                    SqlCommand sqlCommand = new SqlCommand(storeprocedure, sqlConnection);
+                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    sqlCommand.Parameters.AddWithValue("@BookName", bookModel.BookName);
+                    sqlCommand.Parameters.AddWithValue("@AuthorName", bookModel.AuthorName);
+                    sqlCommand.Parameters.AddWithValue("@DiscountPrice", bookModel.DiscountPrice);
+                    sqlCommand.Parameters.AddWithValue("@OriginalPrice", bookModel.OriginalPrice);
+                    sqlCommand.Parameters.AddWithValue("@BookDescription", bookModel.BookDescription);
+                    sqlCommand.Parameters.AddWithValue("@Rating", bookModel.Rating);
+                    sqlCommand.Parameters.AddWithValue("@Reviewer", bookModel.Reviewer);
+                    sqlCommand.Parameters.AddWithValue("@Image", bookModel.Image);
+                    sqlCommand.Parameters.AddWithValue("@BookCount", bookModel.BookCount);
+                    sqlConnection.Open();
+                    int result = sqlCommand.ExecuteNonQuery();
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
