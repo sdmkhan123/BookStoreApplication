0daf8b59456479d710927fd62f7e144174b485d3
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-11 10:27:53 +0530

    [Saddam]Add: Creates BookController classs and Implement Add Book API

---
+++ BookStoreApp/Controller/BookController.cs
@@ -0,0 +1,43 @@
+﻿using BookStoreManager.Interface;
+using BookStoreModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Controller
+{
+    public class BookController : ControllerBase
+    {
+        private readonly IBookManager manager;
+        public BookController(IBookManager manager)
+        {
+            this.manager = manager;
+        }
+        [HttpPost]
+        [Route("api/AddBook")]
+        public IActionResult AddBook([FromBody] BookModel bookmodel)
+        {
+            try
+            {
+                var result = this.manager.AddBook(bookmodel);
+                if (result != 0)
+                {
+
+                    return this.Ok(new { Status = true, Message = "New Book added Successfully !", data = result });
+                }
+                else
+                {
+
+                    return this.BadRequest(new  { Status = false, Message = "Failed to add new book", data = result });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return this.NotFound(new { Status = false, Message = ex.Message });
+            }
+        }
+    }
+}
