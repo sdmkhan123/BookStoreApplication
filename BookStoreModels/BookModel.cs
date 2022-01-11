2146072e1a09e0f8366267fac625d6da483804b5
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-11 10:29:01 +0530

    [Saddam]Add: Creates BookModel classs and Implement BooksModel

---
+++ BookStoreModels/BookModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookStoreModels
+{
+    public class BookModel
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public string BookName { get; set; }
+        [Required]
+        public string AuthorName { get; set; }
+        [Required]
+        public int DiscountPrice { get; set; }
+        [Required]
+        public int OriginalPrice { get; set; }
+        [Required]
+        public string BookDescription { get; set; }
+        [Required]
+        public float Rating { get; set; }
+        [Required]
+        public int Reviewer { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public int BookCount { get; set; }
+    }
+}
