69f338990c1364821c81f53ddb639c3ef1a9a0ae
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-13 00:27:17 +0530

    [Saddam]Add: Created CartModel Class

--- BookStoreModels/CartModel.cs
+++ BookStoreModels/CartModel.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookStoreModels
 {
     public class CartModel
     {
+        [Key]
         public int CartID { get; set; }
+
+        [Required]
         public int UserId { get; set; }
+
+        [Required]
         public int BookId { get; set; }
+
         public int OrderQuantity { get; set; }
+
         public BookModel bookModel { get; set; }
     }
 }
