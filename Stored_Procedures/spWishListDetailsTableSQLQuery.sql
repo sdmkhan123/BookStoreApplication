4a8a1a3490622725b605ccb0cbdbf62123acd33f
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-13 15:58:07 +0530

    [Saddam]Add: Creating wishlist Table and Stored Procedure for Add a book to wishlist API

--- Stored_Procedures/spWishListDetailsTableSQLQuery.sql
+++ Stored_Procedures/spWishListDetailsTableSQLQuery.sql
@@ -1,1 +1,35 @@
 Use BookStoreDB
+
+--************************Implementing CURD Operations************************
+
+--===================================================================================
+--Creating Table of WishLish Details
+--===================================================================================
+create table WishlistTable
+(
+	WishlistId int NOT NULL Identity(1,1) Primary Key,
+	UserId int NOT NULL FOREIGN KEY REFERENCES User_SignUp_Table(UserId),
+	BookId int NOT NULL FOREIGN KEY REFERENCES Book_Details_Table(BookId),	
+)
+--===================================================================================
+--1.Creating Stored Procedure for adding new Book in Cart_Details_Table
+--===================================================================================
+Create PROCEDURE spCreateWishlist
+	@UserId INT,
+	@BookId INT
+AS
+BEGIN
+	Begin Try
+	Begin Transaction
+		IF (EXISTS(SELECT * FROM Book_Details_Table WHERE BookId=@BookId))		
+		begin
+			INSERT INTO WishlistTable VALUES (@UserId,@BookId)
+		end
+	COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+			Rollback TRANSACTION;
+			Select
+				ERROR_MESSAGE() AS ErrorMessage;
+	END CATCH
+END
