679db85b392688c00d5e750ee7d6e44e05f92b23
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-13 15:34:12 +0530

    [Saddam]Add: created wishList model class

---
+++ BookStoreModels/WishlistModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreModels
+{
+    public class WishlistModel
+    {
+
+        [Key]
+        public int WishlistId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        public BookModel Book { get; set; }
+    }
+}
