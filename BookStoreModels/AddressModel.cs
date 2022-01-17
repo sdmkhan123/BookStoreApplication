35387bece5b2ef587263b144f86c4e2ad16d788e
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-18 03:08:02 +0530

    [Saddam]Add: Created Address Model class

---
+++ BookStoreModels/AddressModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookStoreModels
+{
+    public class AddressModel
+    {
+        [Key]
+        public int AddressId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+    }
+}
