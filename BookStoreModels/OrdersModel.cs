f94ab9712dcee8baf44ca320b4b1bedc434a8346
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-19 01:03:51 +0530

    [Saddam]Add: Implemented Orderd Model Class

---
+++ BookStoreModels/OrdersModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreModels
+{
+    public class OrdersModel
+    {
+
+        [Key]
+        public int OrderId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int AddressId { get; set; }
+
+        public int BookId { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public int BookQuantity { get; set; }
+
+        public string OrderDate { get; set; }
+
+        public BookModel bookModel { get; set; }
+    }
+}
