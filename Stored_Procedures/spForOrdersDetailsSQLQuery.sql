b62aeda885fddedd766dd2139dfa21348f1f4334
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-19 01:08:31 +0530

    [Saddam]Add: Created stored Procedure for Adding Order API

--- Stored_Procedures/spForOrdersDetailsSQLQuery.sql
+++ Stored_Procedures/spForOrdersDetailsSQLQuery.sql
@@ -1,1 +1,57 @@
 Use BookStoreDB
+--===================================================================================
+--.Creating Table of Orders Details
+--===================================================================================
+create table OrderTable
+(
+	OrderId int not null identity (1,1) primary key,
+	UserId int not null Foreign Key References User_SignUp_Table(UserId),
+	BookId int not null Foreign Key References Book_Details_Table(BookId),
+	AddressId int not null Foreign Key References Address_Details_Table(AddressId),
+	TotalPrice int,
+	BookQuantity int,
+	OrderDate Date
+)
+--===================================================================================
+--Creating Stored Procedure for Order a Book in Orders_Table
+--===================================================================================
+create procedure spAddingOrders
+	@UserId INT,
+	@AddressId int,
+	@BookId INT ,
+	@BookQuantity int
+AS
+	Declare @TotPrice int
+BEGIN
+	Begin try
+	Begin transaction
+	Select @TotPrice=DiscountPrice from Book_Details_Table where BookId = @BookId;
+	IF EXISTS(SELECT * FROM Book_Details_Table WHERE BookId = @BookId)
+	begin
+		IF EXISTS(SELECT * FROM User_SignUp_Table WHERE UserId = @UserId)
+		Begin			
+			INSERT INTO OrderTable (UserId,AddressId,BookId,TotalPrice,BookQuantity,OrderDate) VALUES
+			( @UserId,@AddressId,@BookId,@BookQuantity*@TotPrice,@BookQuantity,GETDATE())
+
+			Update Book_Details_Table
+			set BookCount=BookCount-@BookQuantity
+
+			Delete from Cart_Details_Table
+			where BookId = @BookId and UserId = @UserId
+		end
+		Else
+		begin
+			Select 1
+		end
+	end 
+	Else
+	begin
+			Select 2
+	end	
+	COMMIT TRANSACTION; 
+	End try
+	Begin catch
+	SELECT  ERROR_MESSAGE() AS ErrorMessage;  
+	ROLLBACK TRANSACTION;  
+	End catch 
+END
