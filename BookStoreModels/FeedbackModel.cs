3a58c9fd5a03b1b569e154780d5f7c63e759d88b
sdmkhan123 <sdmkhan126410@gmail.com>
2022-01-19 16:13:32 +0530

    [Saddam]Add: Implementd feedbackModl class for feedback API

---
+++ BookStoreModels/FeedbackModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookStoreModels
+{
+    public class FeedbackModel
+    {
+		[Key]
+		public int FeedbackId { get; set; }
+		public int UserId { get; set; }
+		public int BookId { get; set; }
+		public string Comments { get; set; }
+		public int Ratings { get; set; }
+		public SignUpModel User { get; set; }
+	}
+}
